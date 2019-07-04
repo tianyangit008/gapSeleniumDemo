a62bc4275a6a57c614d1a74ed45f2da2be27902b
tianyangit008 <49060691+tianyangit008@users.noreply.github.com>
2019-07-04 17:52:06 +0800

    Update case_login.py

--- cases/case_login.py
+++ cases/case_login.py
@@ -7,13 +7,13 @@ class Login(unittest.TestCase):
     def test_login(self):
         """登录"""
         driver=Driver().get_driver()
-        driver.get("http://gap.test.youxinjinrong.com:5200")
+        driver.get("http:")
         driver.maximize_window()
 
         driver.switch_to_frame(0)
         driver.find_element_by_xpath("//ul[@id='loginNav']/li[2]").click()
         driver.find_element_by_name("username").send_keys("tianyan")
-        driver.find_element_by_name("pwd").send_keys("tyQWEasd2019@@")
+        driver.find_element_by_name("pwd").send_keys("**********")
         driver.find_element_by_xpath("//button[@class='login-btn onsign']").click()
 
         time.sleep(5)
